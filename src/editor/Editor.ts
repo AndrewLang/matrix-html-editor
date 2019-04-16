91f334bb144e5ec8c394f531dbb1d1c376fc9a7e
Andy <nnlyx@hotmail.com>
2019-04-16 12:48:07 -0600

    add placeholders

--- src/editor/Editor.ts
+++ src/editor/Editor.ts
@@ -3,6 +3,11 @@ import { IEditor } from './IEditor';
 export class Editor implements IEditor {
     Initialize(selector: string) : IEditor{
         console.log(`Initialize editor: ${selector}`);
+        let element = document.getElementById(selector);
+        if(element){
+            element.contentEditable = 'true';
+        }
+        
         return this;
     }
 
